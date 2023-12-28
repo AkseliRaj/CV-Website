5b102ade59aeb8a6224a30d2b88b9fa41fd44515
AkseliRaj <akseli.rajahalme@gmail.com>
2023-12-28 17:16:30 +0200

    Added content to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
-import '../styles/home.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/home.css'
+
+// Pictures
 import SkeletonPicture1 from '../images/Skeleton.png'
+import SkeletonPicture2 from '../images/Skeleton2.png'
+
 
 function Home() {
   return (
     <div class="container-fluid">
       <div class="row justify-content-center">
-        <div class="col-4 ml-5 align-self-center" id='heading-container'>
+        <div class="col-4 ml-5 align-self-center" id='heading-Container'>
           <div>
             <h1>Moro, minun nimi on Akseli!</h1>
           </div>
@@ -18,8 +22,24 @@ function Home() {
             <h6 id="asterisk-text">*Hieman pidempi livenä mitä olisi voinut odottaa,tai niin olen ainakin kuullut*</h6>
           </div>
         </div>
+        <div class="col-5">
+          <img src={SkeletonPicture1} alt="Skeleton1" class="img-fluid" />
+        </div>
+      </div>
+      <div class="text-center" id="CV-Container">
+        <div>
+          <h1>Jos haluat kokonaisen elämäntarinani, voit ladata CV:ni</h1>
+        </div>
+        <div>
+          <h4>MUTTA</h4>
+        </div>
+        <div>
+          <h4>Huhut kertovat, että olet coolein henkilö, jos koluat sivustoni kokonaan läpi :)</h4>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center" id='picture2-Container'>
         <div class="col-6">
-          <img src={SkeletonPicture1} alt="Skeleton" class="img-fluid"/>
+          <img src={SkeletonPicture2} alt="Skeleton2" class="img-fluid" />
         </div>
       </div>
     </div>
